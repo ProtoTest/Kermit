c4641ea143ee1bb5f11124922d25a4d406661937
Matt <msiwiec@yahoo.com>
2013-08-28 15:17:08 -0600

    Add name to Element class

--- global_scripts/Element.rb
+++ global_scripts/Element.rb
@@ -4,23 +4,25 @@ require 'squish'
 include Squish
 
 class Element
-  attr_reader :symName, :realName, :elementObject
+  attr_reader :name, :symbolicName, :realName, :elementObject
   
-  def initialize(objectString)
+  def initialize(name, objectString)
     begin
+      @name = name
+      
       # if ':' is the first character, then it is a symbolic name
       # if '{' is the first character, then this object is initialized from its real name
       if objectString.start_with?(':')
-        @symName = objectString
-        @realName = ObjectMap.realName(@symName)
+        @symbolicName = objectString
+        @realName = ObjectMap.realName(@symbolicName)
       elsif objectString.start_with?('{') and objectString.end_with?('}')
         @realName = objectString
-        @symName = ObjectMap.symbolicName(@realName)
+        @symbolicName = ObjectMap.symbolicName(@realName)
       else
         raise "objectString is not a valid object string representation"
       end
     
-      @elementObject = findObject(symName)
+      @elementObject = findObject(@symbolicName)
       @properties = Squish::Object.properties(@elementObject)
     rescue Exception => e
       Test.fail(objectString + ": " + e.message)
@@ -31,7 +33,7 @@ class Element
     if @properties[propName]
       return @properties[propName]
     else
-      Test.fail(propName + "property does not exist for element: " + symName)
+      Test.fail(propName + "property does not exist for element: " + @symbolicName)
       return nil
     end
   end
@@ -45,34 +47,6 @@ class Element
   end
   
   def to_s
-    "[symName, realName]: " + @symName + ", " + @realName
-  end
-end
-
-class TextInputElement < Element
-  def initialize(objectString)
-    super objectString
-  end
-  
-  def enterText(text)
-    type(@elementObject, text)
-  end
-  
-  def clear
-    @elementObject.text = ""
-  end
-end
-
-class SliderElement < Element
-  def initialize(objectString)
-    super objectString
-  end
-  
-  def setSliderValue(value)
-    if value.between?(@elementObject.minimum, @elementObject.maximum)
-      @elementObject.sliderPosition = value
-    else
-      raise "Slider value out of bounds exception"
-    end
+    "[name, symbolicName, realName]: " + @name + ", " + @symbolicName + ", " + @realName
   end
 end
