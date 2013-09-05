ba4e84b0f94d4d211bc3f4c49ef76085ea1e3b47
Matt <msiwiec@yahoo.com>
2013-09-05 13:45:27 -0600

    update my playground test case

--- matt/tst_case1/test.rb
+++ matt/tst_case1/test.rb
@@ -2,59 +2,45 @@
 require 'squish'
 include Squish
 
-
+require findFile("scripts", "MainScreen.rb")
 require findFile("scripts", "MainPage.rb")
 require findFile("scripts", "PatientTable.rb")
+require findFile("scripts", "Timer.rb")
 
+def verifyAddTargetScreenDisplayed
+  addTargetButton = Element.new("Add Target", ":Form.Add a Target_QPushButton")
+end
 
 def main
   startApplication("LungAblation")
-    
+  
   # construct a page
   mainPage = MainPage.new
+  pTable = PatientTable.new
+
+  Test.log("Patient list size is :" + pTable.patientList.size.to_s)
   
-  patientContainer = PatientTable.new(":Form.customTreeWidget_CustomTreeWidget")
-  patientObjList = patientContainer.patientList
-  patientObjList.each do |x|
-    Test.log(x.getProperty("text"))
-    x.click
-    snooze 1
+  # Store all the times to load CT images
+  loadCTTimeArray = Array.new
+  
+  # Create a timer
+  myTimer = Timer.new
+  
+  # For each patient in the patient table, load up the CT and time how long it takes.
+  pTable.patientList.each do |x|
+    patientData = x.getDetails
+    patientData.CTRow.dClick()
+    myTimer.start
+    verifyAddTargetScreenDisplayed
+    loadCTTimeArray << [x.name, myTimer.stop]
+    mainPage.loadImagesButton.click
+
   end
   
-#  cElem = Element.new("{column='0' container=':Form.customTreeWidget_CustomTreeWidget' text='1.3.6.1.4.1.9328.50.3.0537' type='QModelIndex'}")
-#  cElem.click()
-  
-#  plan = Element.new("{container=':Form.customTreeWidget_CustomTreeWidget' name='frame' type='QFrame' visible='1'}")
-#  patient = waitForObjectItem(":Form.customTreeWidget_CustomTreeWidget", "3691393543", 2000)
-#  
-#  mouseClick(patient)
-#  
-#  firstRowContainer = waitForObject(":customTreeWidget.frame_QFrame", 2000)
-#  # waitForObjectItem(":customTreeWidget.frame_QFrame", "Create New Plan", 2000) doesn't fucking work
-#  doubleClick(firstRowContainer)
-#  snooze(5)
-#  mainPage.loadImagesButton.click
-#  
-  
-#  :frame.Create New Plan_QPushButton
-#  :frame.Open Plan_QPushButton
-#  :frame.Open Plan_QPushButton_2
-#  {container=':customTreeWidget.frame_QFrame_5' name='openPlanButton' text='Open Plan' type='QPushButton' visible='1'}
-#  {container=':customTreeWidget.frame_QFrame_2' name='openPlanButton' text='Open Plan' type='QPushButton' visible='1'}
-  # First row after patient click
-  #Create new plan row:
-    #:customTreeWidget.frame_QFrame
-    #{container=':Form.customTreeWidget_CustomTreeWidget' name='frame' type='QFrame' visible='1'}
-  #Open plan row: 
-    # :customTreeWidget.frame_QFrame_4
-      #{container=':Form.customTreeWidget_CustomTreeWidget' name='frame' occurrence='2' type='QFrame' visible='1'}
-    # :customTreeWidget.frame_QFrame_5
-      # {container=':Form.customTreeWidget_CustomTreeWidget' name='frame' occurrence='3' type='QFrame' visible='1'}
-    # :customTreeWidget.frame_QFrame_3
-      # {container=':Form.customTreeWidget_CustomTreeWidget' name='frame' occurrence='4' type='QFrame' visible='1'}
-    # :customTreeWidget.frame_QFrame_2
-      # {container=':Form.customTreeWidget_CustomTreeWidget' name='frame' occurrence='5' type='QFrame' visible='1'}
-  
+  loadCTTimeArray.each do |x|
+    Test.log("CT Load Time: " + x.to_s)
+  end
+#
     # 1st child
       ## TEST CASE ##
       # For each patient, Check the CT for respective DICOM data on the load images screen...What data???
@@ -81,22 +67,5 @@ def main
 #  mainPage.birthDateHeaderView.click
 #  mainPage.lastAccessHeaderView.click
   
-#    mainPage.searchBox.enterText("I am searching")
-#    snooze(2)
-#    mainPage.searchBox.clear
-#    mainPage.doShit(mainPage.patient0)
-#    mainPage.captureScreenButton.click
-#    
-#  mainPage.patient0.click
-#  Test.log("patient0 has children") if mainPage.patient0.hasChildren?
-#  mainPage.patient0.printChildren
-  
-  
-    
-    
-  #  mainPage.databaseButton.click
-  #  mainPage.cdButton.click
-  #  mainPage.usbButton.click
-  #  mainPage.hardDriveButton.click
-  #  mainPage.closeButton.click
+
 end
