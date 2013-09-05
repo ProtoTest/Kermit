bade573e55a046644cabdc652d33ed4cce97b792
Matt <msiwiec@yahoo.com>
2013-09-05 12:55:54 -0600

    Refactor the patient table

--- global_scripts/PatientTable.rb
+++ global_scripts/PatientTable.rb
@@ -4,6 +4,36 @@ include Squish
 
 require findFile("scripts", "Element.rb")
 
+########################################################################################
+#
+#  Patient table header component
+#     Stores object elements for the Patient table header
+#
+#  @author  Matt Siwiec
+#  @notes
+#
+########################################################################################
+class TableHeader
+  attr_reader :patientNameHeaderView, :patientIDHeaderView, :birthDateHeaderView, :lastAccessHeaderView
+  
+  def initialize
+    # Table Headers
+    @patientNameHeaderView = Element.new("Patient Name Table Header", ":Patient Name_HeaderViewItem")
+    @patientIDHeaderView = Element.new("Patient ID Table Header", ":Patient ID_HeaderViewItem")
+    @birthDateHeaderView = Element.new("Birth Date Table Header", ":Birth Date_HeaderViewItem")
+    @lastAccessHeaderView = Element.new("Last Accessed Table Header", ":Last Accessed_HeaderViewItem")
+  end
+end
+
+########################################################################################
+#
+#  Cat Scan Row component
+#   Stores object elements of the CT row for a patient
+#
+#  @author Matt Siwiec
+#  @notes Inherits from Element
+#
+########################################################################################
 class CTRow < Element
   attr_reader :icon, :date, :type, :compatibilityLabel, :ratingLabel, :infoLink, :createPlanButton
 
@@ -22,17 +52,18 @@ class CTRow < Element
     @createPlanButton = Element.new("Button", ObjectMap.symbolicName(@children[7]))
     
   end
-  
-  def dClick(timer=nil)
-    doubleClick(waitForObject(@symbolicName)) if Squish::Object.exists(@symbolicName)
-    if timer
-      timer.start
-      Test.log("Timer started")
-    end
-  end
 end
 
-class PlanRow < Element
+########################################################################################
+#
+#  Plan Row component
+#   Stores object elements of a Plan row for a patient
+#
+#  @author Matt Siwiec
+#  @notes Inherits from Element
+#
+########################################################################################
+class PlanRow  < Element
   attr_reader :icon, :type, :date, :planStatusLabel, :planStatusContentsLabel, :byLabel, :byContentsLabel, :openPlanButton
 
   def initialize(objectString)
@@ -55,7 +86,16 @@ class PlanRow < Element
 end
 
 
-
+########################################################################################
+#
+#  Patient Details
+#     Stores the object elements for a Patient's CT and plan rows
+#
+#  @author    Matt Siwiec
+#  @notes     In order to get access to the rows objects, the patient name must be 'clicked'
+#             to open the sub-tree of patient details            
+#
+########################################################################################
 class PatientDetails
   attr_reader :CTRow, :planRows
   
@@ -73,48 +113,83 @@ class PatientDetails
     
     begin
       begin
+        # each patient plan row is indexed via the 'occurrence' property
         rowName = "{container=':Form.customTreeWidget_CustomTreeWidget' name='frame' occurrence='%s' type='QFrame' visible='1'}" % startIndex
        
-        row=findObject(rowName)
+        row=waitForObject(rowName, OBJECT_WAIT_TIMEOUT)
         rows << PlanRow.new(ObjectMap.symbolicName(row))
         startIndex += 1
         
       end while (1)
     rescue Exception => e
+      # don't care, this is used as the condition to break from the while
     end
     
     return rows
   end
 end
 
-class Patient < Element
+########################################################################################
+#
+#  Patient
+#     Stores a reference to the patient element object
+#
+#  @author    Matt Siwiec
+#  @notes     Provides functionality for the tester to get the patient details from the sub-tree
+#
+########################################################################################
+class Patient
+  attr_reader :name
+
   def initialize(name, objectString)
-    super name, objectString
+    @name = name
+    @patientElement = Element.new(name, objectString)
   end
   
-  def click
-    super
+  # Returns the patient details (CT Row, Plan Rows)
+  def getDetails
+    mouseClick(waitForObject(@patientElement.symbolicName))  if Squish::Object.exists(@patientElement.symbolicName)
     return PatientDetails.new
   end
 end
 
 
+########################################################################################
+#
+#  PatientTable
+#     Stores the components of all patients in the table
+#
+#  @author  Matt Siwiec
+#  @notes
+#
+########################################################################################
+class PatientTable
+  attr_reader :tableHeader, :patientList
 
-
-
-def getPatientList
-  patientContainer = Element.new("Patient Container", ":Form.customTreeWidget_CustomTreeWidget")
-  patientList = Array.new
-  
-  patientContainer.children.each do |x|
-    #Test.log(ObjectMap.symbolicName(x))
-    if x.respond_to?(:text) and x.respond_to?(:column) and (x.column==0)
-      # If we are dynamically adding patients after they are loaded, the patient objects
-      # need to be added to the Object Map
-      ObjectMap.add(x)
-      patientList << Patient.new(x.text, ObjectMap.symbolicName(x))
-    end
+  def initialize
+    @tableHeader = TableHeader.new
+    @patientList = getPatientList
   end
-  
-  return patientList
+
+
+  private
+
+  # Returns the list of patients found within the container
+  def getPatientList
+    patientContainer = Element.new("Patient Container", ":Form.customTreeWidget_CustomTreeWidget")
+    patientList = Array.new
+
+    patientContainer.children.each do |x|
+      if x.respond_to?(:text) and x.respond_to?(:column) and (x.column==0)
+        # If we are dynamically adding patients after they are loaded, the patient objects
+        # need to be added to the Object Map
+        ObjectMap.add(x)
+        patientList << Patient.new(x.text, ObjectMap.symbolicName(x))
+      end
+    end
+
+    return patientList
+  end
+
 end
+
