2d6754396e3d73d66af2a88b85713b11cdf3d3d2
Matt <msiwiec@yahoo.com>
2013-09-16 15:35:09 -0600

    Remove findobject, removed click functions

--- global_scripts/Element.rb
+++ global_scripts/Element.rb
@@ -16,52 +16,53 @@ OBJECT_WAIT_TIMEOUT= 5000
 #
 ########################################################################################
 class Element
-  attr_reader :name, :symbolicName, :realName, :children
+  attr_reader :name, :symbolicName, :realName
   
   def initialize(name, objectString)
-    begin
-      @name = name
-      
-      # if ':' is the first character, then it is a symbolic name
-      # if '{' is the first character, then this object is initialized from its real name
-      if objectString.start_with?(':')
-        @symbolicName = objectString
-        @realName = ObjectMap.realName(@symbolicName)
-      elsif objectString.start_with?('{') and objectString.end_with?('}')
-        @realName = objectString
-        @symbolicName = ObjectMap.symbolicName(@realName)
-      else
-        raise "objectString is not a valid object string representation"
-      end
+    @name = name
     
-      elementObject = waitForObject(@symbolicName, OBJECT_WAIT_TIMEOUT)
-      @properties = Squish::Object.properties(elementObject)
-      @children = Squish::Object.children(elementObject)
-    rescue Exception => e
-      Test.fail(objectString + ": " + e.message)
-    end      
+    # if ':' is the first character, then it is a symbolic name
+    # if '{' is the first character, then this object is initialized from its real name
+    if objectString.start_with?(':')
+      @symbolicName = objectString
+      @realName = ObjectMap.realName(@symbolicName)
+    elsif objectString.start_with?('{') and objectString.end_with?('}')
+      @realName = objectString
+      @symbolicName = ObjectMap.symbolicName(@realName)
+    else
+      raise "Element::init(): objectString is not a valid object string representation"
+    end  
   end
     
-  def getProperty(propName)
-    if @properties[propName]
-      return @properties[propName]
-    else
-      Test.log(propName + "property does not exist for element: " + @symbolicName)
+  def getChildren
+    begin
+      elementObject = waitForObject(@symbolicName, OBJECT_WAIT_TIMEOUT)
+      children = Squish::Object.children(elementObject)
+      return children
+    rescue Exception => e
+      Test.fail("Element::getChildren(): " + objectString + ": " + e.message)
       return nil
     end
   end
-
-  def click
-    mouseClick(waitForObject(@symbolicName, OBJECT_WAIT_TIMEOUT))  if Squish::Object.exists(@symbolicName)
+  
+  def getProperty(propName)
+    begin
+      elementObject = waitForObject(@symbolicName, OBJECT_WAIT_TIMEOUT)
+      @properties = Squish::Object.properties(elementObject)
+            
+      if @properties[propName]
+        return @properties[propName]
+      else
+        Test.log("Element::getProperty(): " + propName + "property does not exist for element: " + @symbolicName)
+        return nil
+      end
+    rescue Exception => e
+      Test.fail("Element::getProperty(): " + objectString + ": " + e.message)
+    end
   end
   
-  def dClick
-    doubleClick(waitForObject(@symbolicName, OBJECT_WAIT_TIMEOUT)) if Squish::Object.exists(@symbolicName)
-  end
-  
-  
-    
   def to_s
     "[name, symbolicName, realName]: " + @name + ", " + @symbolicName + ", " + @realName
   end
+  
 end
