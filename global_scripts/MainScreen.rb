5988928df6dd6bbadb52cac9ee91ecb9ea838fea
Matt <msiwiec@yahoo.com>
2013-09-16 15:38:12 -0600

    Cleanup main screen

--- global_scripts/MainScreen.rb
+++ global_scripts/MainScreen.rb
@@ -1,3 +1,7 @@
+# encoding: UTF-8
+require 'squish'
+include Squish
+
 ########################################
 # Seth Urban
 # This is the main screen object child class of the BaseScreenObject
@@ -5,18 +9,20 @@
 #########################################
 # encoding: UTF-8
 require findFile("scripts", "Element.rb")
-require findFile("scripts", "ScreenObject.rb")
+require findFile("scripts", "BaseScreenObject.rb")
+require findFile("scripts", "AppHeaderFooter.rb")
 
 class MainScreen < BaseScreenObject
-  
+  attr_reader :patientTable
   
   def initialize
     
     @searchField = Element.new("SearchField", ":Form.search_QLineEdit")
     @firstPatient = Element.new("FirstPatient", ":customTreeWidget.COVIDIEN PHANTOM 2_QModelIndex")
-    #@firstPatientOpen = Element.new(":frame.Open Plan_QPushButton")
     @covidienLogo = Element.new("CovidienLogo", ":Form.logo_QLabel")
     @statusBar = Element.new("StatusBar", ":Form.statusBarWidget_QWidget")
+    @patientTable = PatientTable.new
+    @appHeaderFooter = AppHeaderFooter.new
    
   end
   
@@ -25,41 +31,23 @@ class MainScreen < BaseScreenObject
     return MainScreen.new
   end
   
-  def openRecord
-    click(@firstPatient)
-    _openPatient = Element.new("FirstPatientOpen", ":frame.Create New Plan_QPushButton")
-    click(_openPatient)
-    _targetOne = Element.new("FirstPatientTargetONe", ":Form.Add a Target_QPushButton")
-    click(_targetOne)
-    _ctAxial = Element.new("AxialTarget", ":Form.qvtkWidget_QVTKWidget")
-    moveTarget(_ctAxial, "LEFT", 50)
-    _editTarget = Element.new("EditTargetOne", ":Form.nameLineEdit_QLineEdit")
-    enterText(_editTarget, "Seth Target")
-    _targetDetails = Element.new("TargetDetail", ":Form.commentsTextEdit_QPlainTextEdit")
-    enterText(_targetDetails, "Here's some stuff to add to the target details thing")
-    _saveTargetButton = Element.new("SaveTrgButton", ":Form.Save Target_QPushButton")
-    click(_saveTargetButton)
-    _saveAblationButton = Element.new("SaveAbButton", ":Form.Save Ablation Zone_QPushButton")
-    click(_saveAblationButton)
-    _saveEntryPoint = Element.new("SaveEntryPoint", ":Form.Save Entry Point_QPushButton")
-    click(_saveEntryPoint)
-    _3DThingy = Element.new("3DModel", ":Form.qvtkWidget_QVTKWidget_6")
-    dragTarget(_3DThingy, "LEFT", 50)
-    return MainScreen.new
+  def clickPatient(patientName)
+    patient = nil
+   @patientTable.patientList.each do |x|
+     if x.name == patientName
+       patient = x
+       break
+     end
+   end
+   
+   if patient.nil?
+     Test.fail("Failed to find patient " + patientName)
+   else
+     return patient.openPatientDetails
+   end
   end
-  
-  def testMousy
-    moveTarget(@covidienLogo)
-    snooze(1)
-    moveTarget(@statusBar)
-    snooze(1)
-    moveTarget(@covidienLogo)
-    snooze(1)
-    moveTarget(@statusBar)
-    snooze(1)
-    moveTarget(@covidienLogo)
-    snooze(1)
-    moveTarget(@statusBar)
-    return MainScreen.new
+
+  def clickLoadImagesRadio
+    click(@appHeaderFooter.loadImagesRadio)
   end
 end
