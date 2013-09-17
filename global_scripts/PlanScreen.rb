abbedae4c84112e068e975e8dceaf48e4b87c997
Matt <msiwiec@yahoo.com>
2013-09-17 11:21:26 -0600

    Rework to support both applications
    in some element object name cases, need to use real name properties
    instead of symbolic name to support both applications

--- global_scripts/PlanScreen.rb
+++ global_scripts/PlanScreen.rb
@@ -22,8 +22,12 @@ class PlanScreen < BaseScreenObject
     @addTargetBtn = Element.new("Add Target", ":Form.Add a Target_QPushButton")
     @changeViewBtn = Element.new("Change View", ":Form.Change Views_QPushButton")
     @captureScreenBtn = Element.new("Capture Screen", ":Form.Capture Screen_QPushButton_2")
-    @loadImagesBtn = Element.new("Load Images", ":Upslope.Load Images_QPushButton_2")
-    @reviewBtn = Element.new("Review", ":Upslope.Review_QPushButton_2")
+    
+    # for these radio buttons, need to use real-property name minus the window property
+    # This is because the Liver and Lung Application main window names differ
+    # "Upslope_MainWindow" vs "Upslope Demo_MainWindow"
+    @loadImagesBtn = Element.new("Load Images", "{name='backButton' text='Load Images' type='QPushButton' visible='1'}")
+    @reviewBtn = Element.new("Review", "{name='nextButton' text='Review' type='QPushButton' visible='1'}")
   end
   
   def clickAddTarget
