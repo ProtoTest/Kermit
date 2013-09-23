6d4a1629010786953497ab8d1c3aef67a20e0d7a
Matt <msiwiec@yahoo.com>
2013-09-23 14:01:22 -0600

    added validation to screen init

--- global_scripts/screen_objects/TargetDetailsScreen.rb
+++ global_scripts/screen_objects/TargetDetailsScreen.rb
@@ -8,9 +8,19 @@ require findFile("scripts", "screen_objects\\BaseScreenObject.rb")
 class TargetDetailsScreen < BaseScreenObject
   def initialize
     @titleLabel = Element.new("Title Label", ":Form.Volume Rendering_QLabel")
-    verifyTextDetails
+
+    @elements = Array.new
+    @elements << @titleLabel
+
+    verifyScreenObjects
   end
   
+
+  def verifyScreenObjects
+  	verifyElementsPresent(@elements)
+    verifyTextDetails
+  end
+
   def verifyTextDetails
       Test.verify("Volume Rendering" == @titleLabel.getProperty('text'), "Target Details title is 'Volume Rendering'")
   end
