c3e62a413bfab8e5a83a6021fc14f2f97e2f756d
Matt <msiwiec@yahoo.com>
2013-10-07 11:33:44 -0600

    updated the endurance tests to use latest framwork updates

--- suite_LiverAblation/tst_Endurance/test.rb
+++ suite_LiverAblation/tst_Endurance/test.rb
@@ -2,14 +2,8 @@
 require 'squish'
 include Squish
 
-require findFile("scripts", "kermit_core\\Timer.rb")
-require findFile("scripts", "screen_objects\\EditAblation.rb")
-require findFile("scripts", "screen_objects\\EditTarget.rb")
+require findFile("scripts", "kermit_core\\TestConfig.rb")
 require findFile("scripts", "screen_objects\\MainScreen.rb")
-require findFile("scripts", "screen_objects\\PatientTable.rb")
-require findFile("scripts", "screen_objects\\AddTargets.rb")
-require findFile("scripts", "screen_objects\\WarningDialogPopup.rb")
-
 
 #
 # Upslope Endurance Test: 
@@ -23,58 +17,7 @@ def main
   # construct the main page
   mainScreen = MainScreen.new
 
-  Test.log("Patient list size is :" + mainScreen.patientTable.getPatientCount.to_s)
-  
-  # For each patient in the patient table, create a plan, then delete it over a period of a day 
-  mainScreen.getPatientList.each_with_index do |patient, index|
-    
-    # There are only MAX_NUM_VISIBLE_TABLE_ROWS visible rows on the Patient Table and Squish will not
-    # automagically select a patient or plan. This script needs to scroll down
-    # to the appropriate table row for Squish to select it
-     
-    # Scroll down to the patient (if necessary) and click on the patient to open details
-    patientDetails = mainScreen.scrollToPatientIndex(index).openPatientDetails(patient)
-
-    # Store the plan count for the patient
-    origPlanCount = patientDetails.getPlanCount
-  
-    # Create a plan for the patient
-    patientDetails.clickCreateNewPlan.clickAddTarget.clickAddAblationZone.clickAddAblation
-    
-    # Go back to load images screen
-    mainScreen.clickLoadImagesRadio
-
-    # Scroll down to the patient (if necessary) and verify new row created
-    patientDetails = mainScreen.scrollToPatientIndex(index).openPatientDetails(patient)
-    
-
-    Test.verify(origPlanCount+1 == patientDetails.getPlanCount, "Verify Plan count increased by one")
-    
-    # Note the current row for the patient
-    patientRowIndex = index % (MAX_NUM_VISIBLE_TABLE_ROWS - 1)
-    Test.log("patientRowIndex: #{patientRowIndex}")
-    
-    newPlanCount = patientDetails.getPlanCount
-    Test.log("New Plan count is " + newPlanCount.to_s)
-
-    # Scroll down to the last plan row to delete it
-    # +1 is for the CT row    
-    mainScreen.scrollToPatientIndex(patientRowIndex + 1 + newPlanCount)
-
-    # Delete the plan
-    patientDetails.planRows.last.deletePlan
-
-    # Open patient details to verify new row created
-    patientDetails = patient.openPatientDetails
-       
-    # Verify the plan was deleted
-    Test.verify(origPlanCount == patientDetails.getPlanCount, "Verify Plan count decreased by one")
-
-    # CLick the row again to close patient details
-    patient.closePatientDetails
-  end
-
-
+  mainScreen.Customer_Endurance_Loop
 
     # 1st child
       ## TEST CASE ##
