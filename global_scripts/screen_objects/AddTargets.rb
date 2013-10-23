38b38780de7ef4d88ea07ffd705d6663e70d5481
Matt <msiwiec@yahoo.com>
2013-10-23 15:24:40 -0600

    added target tab functionality

--- global_scripts/screen_objects/AddTargets.rb
+++ global_scripts/screen_objects/AddTargets.rb
@@ -12,7 +12,7 @@ require findFile("scripts", "screen_objects\\EditTarget.rb")
 #     This screen is presented when users are expecting to add a new target to the plan
 #
 #  @author  Matt Siwiec
-#  @notes -10/04/2013 - SU - Changed all BasePageObject clicks and dclicks and enterText to reference Element directly 
+#  @notes -10/04/2013 - SU - Changed all BasePageObject clicks and dclicks and enterText to reference Element directly
 #
 ########################################################################################
 
@@ -20,13 +20,16 @@ require findFile("scripts", "screen_objects\\EditTarget.rb")
 class AddTargets < BaseScreenObject
   def initialize
     @addTargetBtn = Element.new("Add a Target Button", ":Form.Add a Target_QPushButton")
-    
+
     @appHeaderFooterEdit = AppHeaderFooterEdit.new
 
     @elements = [@addTargetBtn]
     verifyElementsPresent(@elements, self.class.name)
+
+    # used to access the individal target tabs
+    @targetTabsContainer = Element.new("Target Tabs Bar Container", "{container=':Form_MainForm' type='QTabBar' unnamed='1' visible='1'}")
   end
-  
+
   def clickLoadImages
     @appHeaderFooterEdit.clickBackButton
     return MainScreen.new
@@ -41,4 +44,19 @@ class AddTargets < BaseScreenObject
     @addTargetBtn.click
     return EditTarget.new
   end
+
+  def clickTargetTabByName(name)
+    children = @targetTabsContainer.getChildren
+    children.each do |child|
+      if child.respond_to?(:text) and (child.text == name)
+        ObjectMap.add(child)
+        Element.new("Target Tab", ObjectMap.symbolicName(child)).click
+        return EditTarget.new
+      end
+    end
+
+    @@logFile.Fail("Failed to find Target tab for '#{name}'")
+    return nil
+  end
+
 end
