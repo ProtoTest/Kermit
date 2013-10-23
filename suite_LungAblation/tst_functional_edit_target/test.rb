4934dfd67ccc6efc7b66295ae1320bce555b5061
Matt <msiwiec@yahoo.com>
2013-10-23 15:29:16 -0600

    added functional test

---
+++ suite_LungAblation/tst_functional_edit_target/test.rb
@@ -0,0 +1,37 @@
+# encoding: UTF-8
+require 'squish'
+include Squish
+
+require findFile("scripts", "kermit_core\\TestConfig.rb")
+require findFile("scripts", "screen_objects\\MainScreen.rb")
+
+
+def main
+  startApplication("LiverAblation")
+
+  # TestConfig
+  installEventHandlers()
+
+  # construct a page
+  mainScreen = MainScreen.new
+
+  patient_under_test = "1.3.6.1.4.1.9328.50.1.0072"
+  target_name = "Matt's Target"
+  target_note = "As you can see here, automation entered a note for us"
+  mainScreen = mainScreen.
+    clickCreatePlanForPatient(patient_under_test).
+    clickAddTarget.
+    enterTargetInformation(target_name, target_note).
+    clickLoadImages.
+    openPlanForPatient(patient_under_test).
+    clickTargetTabByName(target_name).
+    verifyTargetInformation(target_name, target_note).appHeaderFooterEdit.clickLoadImagesRadio
+
+  # cleanup, delete plan
+  mainScreen.deletePlanForPatient(patient_under_test)
+
+  # test is Done!
+  completeTest
+
+end
+
