7ad70849c25d7594c7727414d405984d309491a0
andar <andartagg@gmail.com>
2013-12-19 16:09:50 -0700

    Change lung ablation endurance test to include importing data from each of the 3 sources.

--- suite_LungAblation/tst_endurance/test.rb
+++ suite_LungAblation/tst_endurance/test.rb
@@ -8,6 +8,14 @@ require findFile("scripts", "screen_objects\\MainScreen.rb")
 #
 # Upslope Endurance Test:
 #
+def runTest
+  1.times do |i|
+    @@logFile.TestLog("STARTING ITERATION #{i}")
+    mainScreen = MainScreen.new
+    mainScreen = mainScreen.Customer_Endurance_Loop
+    @@logFile.TestLog("COMPLETED ITERATION #{i}")
+  end
+end
 
 
 def main
@@ -15,39 +23,49 @@ def main
   startApplication("LungAblation")
 
   # TestConfig
-    installEventHandlers()
+  installEventHandlers()
+  
+  #@@logFile.TestLog("Importing data from HDD")
+  #MainScreen.new.importPatients(:hdd)
+  runTest
+  @@logFile.TestLog("Clearing patient list")
+  MainScreen.new.deletePatients
 
-    # construct the main page, run the test, then end the test
-    mainScreen = nil
+  # CD is E:
+  @@logFile.TestLog("Importing data from HDD")
+  MainScreen.new.importPatients(:cd)
+  runTest
+  @@logFile.TestLog("Clearing patient list")
+  MainScreen.new.deletePatients
 
-    1.times do |i|
-      @@logFile.TestLog("STARTING ITERATION #{i}")
-      mainScreen = MainScreen.new
-      mainScreen = mainScreen.Customer_Endurance_Loop
-      @@logFile.TestLog("COMPLETED ITERATION #{i}")
-    end
+  # USB is K:
+  @@logFile.TestLog("Importing data from HDD")
+  MainScreen.new.importPatients(:usb)
+  runTest
+  @@logFile.TestLog("Clearing patient list")
+  MainScreen.new.deletePatients
 
-    # test is Done!
-    completeTest
+  # test is Done!
+  completeTest
 
-      # 1st child
-        ## TEST CASE ##
-        # For each patient, Check the CT for respective DICOM data on the load images screen...What data???
-          #verify modalityLable exists and has text 'CT'
-          #verify ratingLabel exists and verify x/5 stars (5 different images for each star rating)
-          #verify info link to modal popup
-          #verify 'Create New Plan' button
+    # 1st child
+      ## TEST CASE ##
+      # For each patient, Check the CT for respective DICOM data on the load images screen...What data???
+        #verify modalityLable exists and has text 'CT'
+        #verify ratingLabel exists and verify x/5 stars (5 different images for each star rating)
+        #verify info link to modal popup
+        #verify 'Create New Plan' button
 
-        ## TEST CASE
-        #time how long it takes to load each DICOM image
-        #save the snapshots???
-        #output the times to a file for each patient
+      ## TEST CASE
+      #time how long it takes to load each DICOM image
+      #save the snapshots???
+      #output the times to a file for each patient
 
-        ## TEST CASE
-        # for each patient
-          #click on 'Create New Plan', click on 'Add Target', click on 'Save Target', click on 'Save Ablation Zone'
-            # click on 'Save Entry Point'
-          # Click on 'Load Images' link and verify the plan was created for respective DICOM data
+      ## TEST CASE
+      # for each patient
+        #click on 'Create New Plan', click on 'Add Target', click on 'Save Target', click on 'Save Ablation Zone'
+          # click on 'Save Entry Point'
+        # Click on 'Load Images' link and verify the plan was created for respective DICOM data
 
 
   end
